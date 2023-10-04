19bac216e532964f75b7e7f8742751ded3811635
+Sam Thongo <+samuelthongo73@gmail.com>
2023-10-04 16:23:53 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = [0, 1]
+    for i in range(2, length):
+        my_list.append[my_list[:-1] + my_list[:-2]]
+    return my_list
+    fibonacci sequence = print_fibonacci(10)
+    print(fibonacci sequence)
+        pass
